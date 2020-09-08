c916463e9aadd6ae700aa1c1ed00a9060f91bfaf
Nguen Ngoc Hai <hainn@ip-172-31-35-100.us-east-2.compute.internal>
2020-09-08 07:20:34 +0000

    c1

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "iorder_server"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:hainnsunasterisk/iorder_server.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.cho
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,8 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+set :stage, :production
+set :rails_env, :production
+set :deploy_to, "/deploy/apps/iorder_server"
+set :branch, :config_deploy
+server "3.137.136.122", user: "hainn", roles: %w(web app db)
